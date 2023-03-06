3d041025c091032f6c65f567ce09f8b7fd4396f9
hilal555 <muhamadhilal04@gmail.com>
2023-03-06 15:32:39 +0700

    first commit

---
+++ pr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+//program main
+int main(){//begin
+
+//numeric p,l,luas
+double p,l,luas;
+//display "masukkan panjang"
+cout<<"masukkan panjang = ";
+//accept p
+cin>>p;
+//display "masukkan lebar = ";
+cout<<"masukkan lebar = ";
+//accept L
+cin>>l;
+//compute luas=p*l
+luas=p*l;
+//display "Luas persegi panjang adalah",Luas
+cout<<"luas persegi panjang adalah "<<luas;
+
+}//end
